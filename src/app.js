acdad8441128f68a96ccd84a3060c6204413faf3
Orbytal <orbytal@Mac-Pro-de-Orbytal.local>
2024-01-29 14:52:52 -0500

    base

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const db = require('./utils/database');
+const dotenv = require ('dotenv');
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const userRouter = require('./users/users.router')
+
+
+db.authenticate().
+    then(()=> {
+        console.log('Database Authenticated!');
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+db.sync()
+    .then(()=>{
+        console.log('Database synced');
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Ok!'})
+}) 
+app.use('/api/v1/users', userRouter)
+
+app.listen(port,() => {
+    console.log(`Server started at port ${port}`)
+})
